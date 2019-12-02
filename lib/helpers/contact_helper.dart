8c49a2fc3069f2b9e90267f46cccca7144555760
Mayllon Veras <veras@ifpi.edu.br>
2019-12-02 20:49:11 -0300

    create Database

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -1,12 +1,43 @@
 import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
+final String contactTable = "contactTable";
 final String idColumn = "idColumn";
 final String nameColumn = "nameColumn";
 final String emailColumn = "emailColumn";
 final String phoneColumn = "phoneColumn";
 final String imageColumn = "imageColumn";
 
-class ContactHelper {}
+class ContactHelper {
+  static final ContactHelper _instance = ContactHelper.internal();
+
+  factory ContactHelper() => _instance;
+
+  ContactHelper.internal();
+
+  Database _database;
+
+  Future<Database> get database async {
+    Future<Database> initDatabase() async {
+      final databasesPath = await getDatabasesPath();
+      final path = join(databasesPath, "contact.db");
+
+      return await openDatabase(path, version: 1,
+          onCreate: (Database db, int newerVersion) async {
+        await db
+            .execute("CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY,"
+                "$nameColumn TEXT, $nameColumn TEXT, $emailColumn TEXT,"
+                "$phoneColumn TEXT, $imageColumn TEXT)");
+      });
+    }
+
+    if (_database != null) {
+      return _database;
+    } else {
+      _database = await initDatabase();
+    }
+  }
+}
 
 class Contact {
   int id;

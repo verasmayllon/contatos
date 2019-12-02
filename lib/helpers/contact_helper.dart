0da74e0d31d8fbf490ee960b31530a0ba8fcec8b
Mayllon Veras <veras@ifpi.edu.br>
2019-12-02 20:01:27 -0300

    Class Contact

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -1,14 +1,41 @@
 import 'package:sqflite/sqflite.dart';
 
-class ContactHelper{
+final String idColumn = "idColumn";
+final String nameColumn = "nameColumn";
+final String emailColumn = "emailColumn";
+final String phoneColumn = "phoneColumn";
+final String imageColumn = "imageColumn";
 
-}
+class ContactHelper {}
 
-class Contact{
+class Contact {
   int id;
   String name;
   String email;
   String phone;
   String image;
 
+  Contact.fromMap(Map map) {
+    this.id = map[idColumn];
+    this.name = map[nameColumn];
+    this.email = map[emailColumn];
+    this.phone = map[phoneColumn];
+    this.image = map[imageColumn];
+  }
+
+  Map toMap() {
+    Map<String, dynamic> map = {
+      nameColumn: this.name,
+      emailColumn: this.email,
+      phoneColumn: this.phone,
+      imageColumn: this.image,
+    };
+    if (idColumn != null) map[idColumn] = this.id;
+    return map;
+  }
+
+  @override
+  String toString() {
+    return "Contact(id: $id, name: $name, email: $email, phone: $phone, image: $image)";
+  }
 }
